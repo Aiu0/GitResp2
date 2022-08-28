1ef63a2659f8b66320c8405b9fd834a5ab747ba0
gx <chinazhaoss@123.com>
2022-08-28 14:42:30 +0800

    在Person类中增加了name,age,weight属性

---
+++ TestModule1/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: GaoX
+ * @Date: 2022/8/28 - 08 - 28 - 14:39
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+    private String name;
+    private double height;
+}
